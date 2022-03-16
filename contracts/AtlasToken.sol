842a0f6aecca1107abf16061af7b8beba0d32dd4
Kase Lunt <kllunt@asu.edu>
2022-03-15 23:09:49 -0700

    initial commit

--- contracts/AtlasToken.sol
+++ contracts/AtlasToken.sol
@@ -2,8 +2,8 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract FarmToken is ERC20 {
-    constructor() public ERC20("Farm Token", "FARM") {
+contract AtlasToken is ERC20 {
+    constructor() public ERC20("Atlas Token", "ATLAS") {
         _mint(msg.sender, 1000000000000000000000000);
     }
 }
